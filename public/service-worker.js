8de6b0ba25a7f65a60d6ac2810b9a275aa3b7a7e
Ajay Singh bhandari <ajaybhandari0064@gmail.com>
2023-12-06 15:50:06 +0530

    revamp servicewrker

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,33 +13,10 @@ self.addEventListener('install', event => {
   );
 });
 
-// self.addEventListener('fetch', event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
 self.addEventListener('fetch', event => {
-  if (
-    event.request.url.startsWith('https://ecom-node-server.onrender.com/api/')
-  ) {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          // Optionally, you can add logic here to cache the API responses if needed.
-          return response;
-        })
-        .catch(error => {
-          console.error('Error fetching API:', error);
-        })
-    );
-  } else {
-    // For other requests, use cache-first strategy
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
