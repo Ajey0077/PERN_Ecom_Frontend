bafea4590575187b07d83b4e26dba68a270f1146
Ajay Singh bhandari <ajaybhandari0064@gmail.com>
2023-12-07 11:27:55 +0530

    debugging in prod

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,19 +4,24 @@ const urlsToCache = [
   '/index.html',
   // Add other static assets and HTML files
 ];
+console.log('public service worker');
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+try {
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+} catch (err) {
+  console.log('🚀 ~ file: service-worker.js:27 ~ err:', err);
+}
